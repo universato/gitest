361186607d404371a4f17241a94d7feac722fc19
universato <univesato@gmail.com>
2020-11-09 17:33:03 +0900

    add temp 3

--- src/example/test.rb
+++ src/example/test.rb
@@ -1,3 +1,18 @@
 puts "test"
+
+puts "hank 0"
+
 puts "add-tmp"
+
+p "gorira"
+
 puts "add-tmp2"
+puts "add-tmp3"
+
+puts "add-hank"
+
+puts "hello world"
+
+def test
+end
+
---
+++ src/example/test2.rb
@@ -0,0 +1,2 @@
+afghfnggff
+after add
